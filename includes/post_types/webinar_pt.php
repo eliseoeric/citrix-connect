f2a8f163e2bd0efa87895fa28613e27977a6bd69
eliseoeric <eric.eliseo@gmail.com>
2016-01-24 20:06:47 -0500

    change url structure

--- includes/post_types/webinar_pt.php
+++ includes/post_types/webinar_pt.php
@@ -26,8 +26,8 @@ function register_webinars() {
                 'query_var'           => true,
                 'menu_position'       => 8, /* this is what order you want it to appear in on the left hand side menu */
                 'menu_icon'           => 'dashicons-images-alt2', /* the icon for the custom post type menu */
-               'rewrite'             => array ( 'slug' => 'webinar', 'with_front' => false ), /* you can specify its url slug */
-                'has_archive'         => 'webinar-courses', /* you can rename the slug here */
+               'rewrite'             => array ( 'slug' => 'webinars', 'with_front' => false ), /* you can specify its url slug */
+                'has_archive'         => 'webinars', /* you can rename the slug here */
                 'capability_type'     => 'post',
                 'hierarchical'        => false,
             /* the next one is important, it tells what's enabled in the post editor */
